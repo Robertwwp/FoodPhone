12e9b9bc2669aa8d65351752b4f7bfbfe01133af
Robertwwp <wwpliv@gmail.com>
2018-12-10 22:07:39 -0500

    bug fix

--- finaltrain_prepare.py
+++ finaltrain_prepare.py
@@ -1,20 +1,25 @@
 import FunctionUnits_sk as FU
 from skimage.transform import resize
-from skimage import io, color, exposure
-from skimage.feature import hog
+from skimage import io, color, exposure, img_as_ubyte
+from skimage.feature import hog, greycomatrix, greycoprops
 import numpy as np
 import time
 
-def BGRCues_img(img):
-    return np.mean(img,axis=(0,1))
+def BGRCues_img(img, area):
+    if np.max(img) <= 1:
+        return np.sum(img,axis=(0,1))/area
+    else:
+        return np.sum(img,axis=(0,1))/(255*area)
 
-def HSVCues_img(img):
+def HSVCues_img(img, area):
     hsvimg = color.rgb2hsv(img)
-    return np.mean(hsvimg,axis=(0,1))
+    return np.sum(hsvimg,axis=(0,1))/area
 
 def HistCues_img(greyimg, area):
-    hist5 = exposure.histogram(greyimg, nbins=5)[0]/area
-    hist3 = exposure.histogram(greyimg, nbins=3)[0]/area
+    cond = greyimg!=0
+    image = np.extract(cond, greyimg)
+    hist5 = exposure.histogram(image, nbins=5)[0]/area
+    hist3 = exposure.histogram(image, nbins=3)[0]/area
 
     return hist5, hist3
 
@@ -23,6 +28,23 @@ def HoG(img):
              cells_per_block=(1, 1), feature_vector=True, multichannel=True)
 
     return fd
+
+def GLCM(greyimg):
+    coords = np.argwhere(greyimg != 0)
+    glcm = np.zeros((9,16))
+    greyimg = img_as_ubyte(greyimg)
+    for i in range(1,10):
+        x, y = coords[int(0.1*i*len(coords))]
+        if x < 10:
+            x = 10
+        if y < 10:
+            y = 10
+        image = greyimg[x-10:x+11,y-10:y+11]
+        matrix = greycomatrix(image,[1,2],[0,np.pi/4,np.pi/2,3*np.pi/4],levels=256,normed=True)
+        glcm[i-1,:8] = greycoprops(matrix, 'dissimilarity').ravel()
+        glcm[i-1,8:16] = greycoprops(matrix, 'correlation').ravel()
+
+    return np.mean(glcm,axis=0)
 
 def multiappend(seq_features):
     result = seq_features[0]
@@ -34,9 +56,13 @@ def multiappend(seq_features):
 
 if __name__ == '__main__':
 
+    """img = io.imread("t1.jpg")
+    greyimg = color.rgb2grey(img)
+    GLCM(greyimg)"""
+
     alltypes = open("classes.txt").read().splitlines()
     D = {}
-    train = np.zeros((75750,2062))
+    train = np.zeros((75750,30))
     labels = np.zeros(75750)
     count = 0
     for type in alltypes:
@@ -59,8 +85,8 @@ if __name__ == '__main__':
                 if area < 32768:
                     pass
                 else:
-                    BGR,HSV,(Hist5,Hist3),fd = BGRCues_img(img),HSVCues_img(img),HistCues_img(greyimg,area),HoG(img)
-                    train[count,:] = multiappend([BGR, HSV, Hist5, Hist3, fd])
+                    BGR,HSV,(Hist5,Hist3),glcm = BGRCues_img(img,area),HSVCues_img(img,area),HistCues_img(greyimg,area),GLCM(greyimg)
+                    train[count,:] = multiappend([BGR, HSV, Hist5, Hist3, glcm])
                     labels[count] = D[name]
                     count = count + 1
             except:
@@ -73,12 +99,12 @@ if __name__ == '__main__':
             print(filecount)
             print("time spent (min):")
             print((time.time()-s)/60)
-            np.save("train", train)
-            np.save("labels", labels)
+            np.save("train_glcm", train)
+            np.save("labels_glcm", labels)
 
     print("valid image processed:")
     print(count)
     print("time spent (min):")
     print((time.time()-s)/60)
-    np.save("train", train)
-    np.save("labels", labels)
+    np.save("train_glcm", train)
+    np.save("labels_glcm", labels)
